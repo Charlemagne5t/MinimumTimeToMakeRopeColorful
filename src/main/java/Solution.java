6e3789c2a8ed8df032ce6487fb0c46c3be36edbc
alek <wait82985@gmail.com>
2023-12-27 11:12:50 +0600

    correct bug

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -4,9 +4,8 @@ public class Solution {
         int cost = 0;
 
         for (int i = 1; i < col.length; i++){
-            if(col[i - 1] == col[i]){
-                if(col[i] == col[i - 1])
-                    cost += Math.min( neededTime[i], neededTime[i - 1]);
+            if(col[i] == col[i - 1]){
+                cost += Math.min( neededTime[i], neededTime[i - 1]);
                 neededTime[i] = Math.max(neededTime[i], neededTime[i - 1]);
                 col[i - 1] = '*';
             }
